f88743ccad91fc7a5229c6ff7c93e1aa094f6a87
Maria Bazhlekova <mbazhlek@gmail.com>
2021-04-06 09:01:32 -0700

    chore: cors got me again

--- comments/index.js
+++ comments/index.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const { randomBytes } = require("crypto");
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId?.[req.params.id] ?? []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const commentId = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId?.[req.params.id] ?? [];
@@ -24,5 +26,5 @@ app.post("/posts/:id/comments", (req, res) => {
 });
 
 app.listen(4001, () => {
-  console.log("magic happening on port 4001");
+  console.log('magic happening on port 4001');
 });
--- posts/index.js
+++ posts/index.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const { randomBytes } = require("crypto");
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
     id,
@@ -22,5 +24,5 @@ app.post("/posts", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log("Listening on 4000");
+  console.log('Listening on 4000');
 });
